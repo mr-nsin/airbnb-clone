33b24948eb3fff36953090a30adce0329572b393
Nitin Singhal <singhal.nitin94@gmail.com>
2023-09-15 01:16:33 +0530

    Added badge of sold out in div tag

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App(){
   
   const cards = Data.map(obj => {
     return <Card 
+        key={obj.id}
         img={obj.coverImg}
         rating={obj.stats.rating}
         reviewCount={obj.stats.reviewCount}
@@ -19,7 +20,9 @@ export default function App(){
       <div>
         <Navbar />
         {/* <Hero /> */}
-        {cards}
+        <section className="cards--list">
+          {cards}
+        </section>
       </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ export default function Card(props){
     // console.log(props)
     return (
         <div className="card">
+            <div className="card--badge">SOLD OUT</div>
             <img src={require(`../images/${props.img}`)} className="card--image"/>
             <div className="card--stats">
                 <img src={require('../images/star.png')} className="card--star"/>
@@ -11,8 +12,8 @@ export default function Card(props){
                 <span className="gray">({props.reviewCount}) * </span>
                 <span className="gray">{props.country}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
