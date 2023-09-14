12c7305ca1370cb32980c44d60edcf42843095c4
Nitin Singhal <singhal.nitin94@gmail.com>
2023-09-15 00:02:51 +0530

    Use map object to create mulitple cards returning JSX array of elements

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from "react";
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import Data from "./data"
 
 export default function App(){
+  
+  const cards = Data.map(obj => {
+    return <Card 
+        img={obj.coverImg}
+        rating={obj.stats.rating}
+        reviewCount={obj.stats.reviewCount}
+        country={obj.location}
+        title={obj.title}
+        price={obj.price}/>
+  })
   return (
       <div>
         <Navbar />
         {/* <Hero /> */}
-        <Card 
-            img="katie-zaferes.png"
-            rating="5.0"
-            reviewCount={6}
-            country="USA"
-            title="Life Lessons with katie zaferes"
-            price={136}/>
+        {cards}
       </div>
   )
 }
