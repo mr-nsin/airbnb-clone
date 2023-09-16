b279c2b4855530e77efcc5ac675e9ee1a8dada92
Nitin Singhal <singhal.nitin94@gmail.com>
2023-09-16 21:58:28 +0530

    Revert back hero component to show whole components

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App(){
   return (
       <div>
         <Navbar />
-        {/* <Hero /> */}
+        <Hero />
         <section className="cards--list">
           {cards}
         </section>
